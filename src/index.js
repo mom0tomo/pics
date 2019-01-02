5c9ec5437018350bee7a4e111457d928c370bf91
mom0tomo <momo76w@gmail.com>
2019-01-02 22:32:02 +0900

    Initial commit from Create React App

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Segment from './Segment';
+
+const App = () => {
+	return <div>Hi </div>;
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
